bd587dce62c6bb54b2f9e86a2f791f6787302017
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2022-12-04 02:03:48 -0300

    feat: added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { MenuItem } from 'primeng/api';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tenchi-challenge';
+  items!: MenuItem[];
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              items: [{
+                      label: 'New',
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {label: 'Project'},
+                          {label: 'Other'},
+                      ]
+                  },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              ]
+          }
+      ];
+  }
 }
